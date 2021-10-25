8106ec18e1b0dfdc7234d77b2856d1c54dcb582e
De Jonghe Stef <stef.de.jonghe@student.howest.be>
2021-10-25 12:16:43 +0200

    Add Genre entity to core project

---
+++ Pri.Spoticlone.Core/Entities/Genre.cs
@@ -0,0 +1,15 @@
+﻿using Pri.Spoticlone.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Spoticlone.Core.Entities
+{
+    public class Genre : EntityBase
+    {
+        public string Name { get; set; }
+        public ICollection<ArtistGenre> ArtistGenres { get; set; }
+    }
+}
