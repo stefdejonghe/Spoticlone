75afc0eadb8639e0080760d98f8c277bc3cb3e61
De Jonghe Stef <stef.de.jonghe@student.howest.be>
2021-10-25 12:12:02 +0200

    Add Album entity to core project

---
+++ Pri.Spoticlone.Core/Entities/Album.cs
@@ -0,0 +1,18 @@
+﻿using Pri.Spoticlone.Core.Entities.Base;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Pri.Spoticlone.Core.Entities
+{
+    public class Album : EntityBase
+    {
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public Uri Image { get; set; }
+        public Guid ArtistId { get; set; }
+        public string SpotifyId { get; set; }
+        public Artist Artist { get; set; }
+        public ICollection<Track> MyProperty { get; set; }
+    }
+}
