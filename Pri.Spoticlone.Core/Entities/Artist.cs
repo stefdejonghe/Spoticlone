2fd7efaf81b9bc1b1b03557a28175f1c0c6e0c8d
De Jonghe Stef <stef.de.jonghe@student.howest.be>
2021-10-25 12:08:21 +0200

    Add Artist entity to core project

---
+++ Pri.Spoticlone.Core/Entities/Artist.cs
@@ -0,0 +1,20 @@
+﻿using Pri.Spoticlone.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Spoticlone.Core.Entities
+{
+    public class Artist : EntityBase
+    {
+        public string Name { get; set; }
+        public int Followers { get; set; }
+        public int Popularity { get; set; }
+        public ICollection<ArtistGenre> ArtistGenres { get; set; }
+        public ICollection<Album> Albums { get; set; }
+        public string SpotifyId { get; set; }
+        public Uri Image { get; set; }
+    }
+}
