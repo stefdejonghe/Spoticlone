fc3683e5664297b19568e4c0d558bf032f365310
De Jonghe Stef <stef.de.jonghe@student.howest.be>
2021-10-25 12:18:27 +0200

    Add ArtistGenre entity to core project

---
+++ Pri.Spoticlone.Core/Entities/ArtistGenre.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pri.Spoticlone.Core.Entities
+{
+    public class ArtistGenre
+    {
+        public Guid ArtistId { get; set; }
+        public Guid GenreId { get; set; }
+        public Artist Artist { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
