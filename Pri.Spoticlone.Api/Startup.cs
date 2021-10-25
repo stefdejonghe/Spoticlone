8f55d65ce23b36c6697eb573ed82fd6f48afedd2
De Jonghe Stef <stef.de.jonghe@student.howest.be>
2021-10-25 12:44:08 +0200

    Add DbContext to Startup class

--- Pri.Spoticlone.Api/Startup.cs
+++ Pri.Spoticlone.Api/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Pri.Spoticlone.Infrastructure.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,7 @@ namespace Pri.Spoticlone.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DatabaseConnection")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
