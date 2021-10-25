95f75ddef6eef93d51086cb690e585e5962cb8c9
De Jonghe Stef <stef.de.jonghe@student.howest.be>
2021-10-25 12:27:54 +0200

    Add many to many relation Artist and Genre

---
+++ Pri.Spoticlone.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pri.Spoticlone.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Spoticlone.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+        public DbSet<ArtistGenre> ArtistGenres { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ArtistGenre>()
+                .ToTable("ArtistGenre")
+                .HasKey(ag => new { ag.ArtistId, ag.GenreId });
+
+            modelBuilder.Entity<ArtistGenre>()
+                .HasOne(ag => ag.Artist)
+                .WithMany(g => g.ArtistGenres)
+                .HasForeignKey(ag => ag.ArtistId);
+
+            modelBuilder.Entity<ArtistGenre>()
+                .HasOne(ag => ag.Genre)
+                .WithMany(a => a.ArtistGenres)
+                .HasForeignKey(ag => ag.GenreId);
+        }
+    }
+}
