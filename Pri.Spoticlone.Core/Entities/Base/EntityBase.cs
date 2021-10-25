aaee6d0b157c047764a9288ad77481b5cec492cf
De Jonghe Stef <stef.de.jonghe@student.howest.be>
2021-10-25 12:03:54 +0200

    Add base entity to core project

---
+++ Pri.Spoticlone.Core/Entities/Base/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Spoticlone.Core.Entities.Base
+{
+    public abstract class EntityBase
+    {
+        public Guid Id { get; set; }
+    }
+}
