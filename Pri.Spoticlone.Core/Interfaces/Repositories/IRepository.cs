9c8b9a221468eb785bb08c375f41710216e65bab
De Jonghe Stef <stef.de.jonghe@student.howest.be>
2021-11-04 23:19:04 +0100

    Add generic interface IRepository

---
+++ Pri.Spoticlone.Core/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using Pri.Spoticlone.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Spoticlone.Core.Interfaces.Repositories
+{
+    public interface IRepository<T> where T : EntityBase
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task<T> GetByIdAsync(Guid id, string[] includes);
+        IQueryable<T> GetAllAsync();
+        Task<IEnumerable<T>> ListAllAsync();
+        IQueryable<T> GetFiltered(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> ListFiltered(Expression<Func<T, bool>> predicate);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+        Task<T> DeleteAsync(Guid id);
+    }
+}
