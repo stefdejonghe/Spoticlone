5d75d9a8dc1ad0bb71c40686b69eb319f3c50e14
De Jonghe Stef <stef.de.jonghe@student.howest.be>
2021-11-04 23:35:32 +0100

    Add Artist- and GenreResponseDto's to project

---
+++ Pri.Spoticlone.Core/Dtos/ArtistResponseDto.cs
@@ -0,0 +1,20 @@
+﻿using Pri.Spoticlone.Core.Dtos.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Spoticlone.Core.Dtos
+{
+    public class ArtistResponseDto : DtoBase
+    {
+        public string Name { get; set; }
+        public int Followers { get; set; }
+        public int Popularity { get; set; }
+        public string SpotifyId { get; set; }
+        public Uri Image { get; set; }
+        public ICollection<GenreResponseDto> Genres { get; set; }
+        public int AlbumCount { get; set; }
+    }
+}
---
+++ Pri.Spoticlone.Core/Dtos/GenreResponseDto.cs
@@ -0,0 +1,9 @@
+﻿using Pri.Spoticlone.Core.Dtos.Base;
+
+namespace Pri.Spoticlone.Core.Dtos
+{
+    public class GenreResponseDto : DtoBase
+    {
+        public string Name { get; set; }
+    }
+}
