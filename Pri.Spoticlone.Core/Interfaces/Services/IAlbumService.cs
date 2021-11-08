ef7bc41809b95067092de7db0082a742cbef6207
De Jonghe Stef <stef.de.jonghe@student.howest.be>
2021-11-08 10:10:25 +0100

    Add IAlbumService interface

---
+++ Pri.Spoticlone.Core/Interfaces/Services/IAlbumService.cs
@@ -0,0 +1,15 @@
+﻿using Pri.Spoticlone.Core.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Spoticlone.Core.Interfaces.Services
+{
+    public interface IAlbumService
+    {
+        Task<IEnumerable<AlbumResponseDto>> ListAllAsync();
+        Task<AlbumResponseDto> GetByIdAsync(Guid id);
+    }
+}
