9b671c43868ef6b9a2a84cbcdbea00373a47c7e6
De Jonghe Stef <stef.de.jonghe@student.howest.be>
2021-11-08 09:58:28 +0100

    Add TrackResponseDto to project

---
+++ Pri.Spoticlone.Core/Dtos/TrackResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using Pri.Spoticlone.Core.Dtos.Base;
+using System;
+
+namespace Pri.Spoticlone.Core.Dtos
+{
+    public class TrackResponseDto : DtoBase
+    {
+        public string Name { get; set; }
+        public string SpotifyId { get; set; }
+        public int TrackNumber { get; set; }
+        public int DiscNumber { get; set; }
+        public int DurationMs { get; set; }
+        public bool Explicit { get; set; }
+        public Uri PreviewUrl { get; set; }
+        public Guid AlbumId { get; set; }
+        public AlbumResponseDto Album { get; set; }
+    }
+}
