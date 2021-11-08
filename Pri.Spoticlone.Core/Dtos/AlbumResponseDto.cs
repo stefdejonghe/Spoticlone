7c9761824518b8a8f2edc4344cbad8b14a0684cb
De Jonghe Stef <stef.de.jonghe@student.howest.be>
2021-11-08 09:58:38 +0100

    Add AlbumResponseDto to project

---
+++ Pri.Spoticlone.Core/Dtos/AlbumResponseDto.cs
@@ -0,0 +1,20 @@
+﻿using Pri.Spoticlone.Core.Dtos.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Spoticlone.Core.Dtos
+{
+    public class AlbumResponseDto : DtoBase
+    {
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public Uri Image { get; set; }
+        public Guid ArtistId { get; set; }
+        public string SpotifyId { get; set; }
+        public ArtistResponseDto Artist { get; set; }
+        public int TrackCount { get; set; }
+    }
+}
