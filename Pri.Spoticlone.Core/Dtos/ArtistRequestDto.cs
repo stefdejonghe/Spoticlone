c4a2978328814116a432a742465dca520bed5c92
De Jonghe Stef <stef.de.jonghe@student.howest.be>
2021-11-08 14:55:06 +0100

    Add ArtistRequestDto to project

---
+++ Pri.Spoticlone.Core/Dtos/ArtistRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using Pri.Spoticlone.Core.Dtos.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Spoticlone.Core.Dtos
+{
+    public class ArtistRequestDto : DtoBase
+    {
+        public string Name { get; set; }
+        public int Followers { get; set; }
+        public int Popularity { get; set; }
+        public Uri Image { get; set; }
+    }
+}
