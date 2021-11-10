cf941f1a86678a9021d2e55d77447b0e26561441
De Jonghe Stef <stef.de.jonghe@student.howest.be>
2021-11-10 10:35:19 +0100

    Add AlbumRepository to project

---
+++ Pri.Spoticlone.Infrastructure/Repositories/AlbumRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pri.Spoticlone.Core.Entities;
+using Pri.Spoticlone.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Spoticlone.Infrastructure.Repositories
+{
+    public class AlbumRepository : EfRepository<Album>
+    {
+        public AlbumRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        public override async Task<Album> GetByIdAsync(Guid id)
+        {
+            return await GetAllAsync().SingleOrDefaultAsync(a => a.Id.Equals(id));
+        }
+
+        public override IQueryable<Album> GetAllAsync()
+        {
+            return _dbContext.Albums.AsNoTracking()
+                .Include(a => a.Artist)
+                .Include(a => a.Tracks);
+        }
+    }
+}
